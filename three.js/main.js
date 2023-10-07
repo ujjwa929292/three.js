8b5f8c0b0bdac878350e5f5c2d010ae45f3f958d
Ujjwal Vij <uvij027@gmail.com>
2023-10-07 23:47:01 +0530

    push

--- three.js/main.js
+++ three.js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import './style.css';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
+import gsap from 'gsap';
 
 //Scene
 const scene = new THREE.Scene();
@@ -11,6 +11,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
+  roughness: 0.5
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -66,3 +67,33 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 }
 loop();
+
+//timeline 
+const tl = gsap.timeline({defaults: {duration:1}});
+tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {x:1, y:1, z:1});
+tl.fromTo('nav' , {y: "-100%"} , {y: "0%"});
+tl.fromTo(".title", {opacity: 0}, {opacity: 1});
+
+
+//Mouse Animation Color
+let mouseDown = false;
+let rgb = []
+window.addEventListener('mousedown',() => (mouseDown = true) );
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    //Lets Animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {
+      r: newColor.r, 
+      g: newColor.g, 
+      b: newColor.b,
+    })
+  }
+});
